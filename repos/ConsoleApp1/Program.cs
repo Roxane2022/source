b08f0807b515238f07ef564cd4a29b04e2004a5e
Roxane2022 <cecile.prigge@docc.techstarter.de>
2023-03-23 10:21:57 +0100

    first commit

---
+++ repos/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+if ('1' == 1)
+{
+    Console.WriteLine("1=1");
+    
+}
+else Console.WriteLine("Nicht gleich")
---
+++ repos/Script1.js
@@ -0,0 +1,35 @@
+// JavaScript source code
+a = 1;
+b = 1;
+
+if ("1" === 1) {
+    console.log("test equal 1==1");
+
+
+}
+if (null === undefined) {
+    console.log("test equal 1==1");
+
+}
+
+let fruechte = [];
+fruechte.push('Banane', 'Apfel', 'Birne');
+fruechte.push('Kartofel');
+
+
+let temp = [];
+fruechte.splice(0, 2);
+console.log(fruechte.pop());
+
+
+
+console.log(fruechte.length);
+console.log(fruechte);
+
+let einkaufwagen = [];
+function nehmen() {
+
+    einkauwagen.push(fruechte.pop());
+    consolo.log(einkaufwagen.toString());
+
+}
